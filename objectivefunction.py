1ee4dc5c071cde968c04403d4f234e02cdd07b42
peregrinus <retiarus@gmail.com>
2018-04-21 21:53:03 -0300

    correct objective function, add more entrys to .gitignore

--- objectivefunction.py
+++ objectivefunction.py
@@ -1,21 +1,16 @@
 import numpy as np
-from itertools import islice, count
 
 class FuncJ(object):
-    def __init__(self, alpha, A, regularizationFunction = None):
-        self.__alpha = alpha
-        self.__A = A
-                 
-        if regularizationFunction is None:
-            self.__regularizationFunction = tikhonovorder0(A.shape[0])
-        else:
-            self.__regularizationFunction = regularizationFunction
-            
+    def __init__(self, alpha, A, regularization_function):
+        self._alpha = alpha
+        self._A = A
+        self._regularization_function = regularization_function
+
     def __call__(self, f, fmedido):
-        if self.J:
-            return np.sum((self.__A(f) - fmedido)**2.0) + self.__alpha*self.__regularizationFunction(f)
-        else:
-            return None
-        
-    def changeAlpha(self, alpha):
-        self.__alpha = alpha
+        return (np.sum((self._A(f) - fmedido)**2.0) +
+                self._alpha*self._regularization_function(f))
+
+    @property
+    def alpha(self, alpha):
+        ''' set alpha parameter '''
+        self._alpha = alpha
